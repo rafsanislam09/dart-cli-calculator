425f1c7b4f1fe5b2b9c4586e8fd9fc30ec472abc
rafsanislam09 <rafsan.islam010@gmail.com>
2024-12-22 15:00:31 +0600

    Created Dart Cli Calculator

---
+++ bin/dart_cli_calculator.dart
@@ -0,0 +1,3 @@
+import 'package:dart_cli_calculator/src/main.dart' as dart_cli_calculator;
+
+void main() => dart_cli_calculator.main();
---
+++ test/dart_cli_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_cli_calculator/src/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    
+  });
+}
